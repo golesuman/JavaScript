bbb596a22f734a16186266bd66465f5b85eeaa6a
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-24 20:31:56 +0545

    operators in JS

---
+++ Basics/operators.js
@@ -0,0 +1,13 @@
+let a = 5
+let b = 10
+
+console.log(a+b);
+console.log(a == b);
+console.log(a*b);
+console.log(b/a);
+console.log(b%a);
+console.log(a**2);
+console.log(a+=1);
+console.log(b++)
+console.log(b)
+// console.log(++b)
