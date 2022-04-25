c374100cba815d4c6a40149440b5e623821f23d9
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-25 20:39:17 +0545

    Logical operators in JS

---
+++ Basics/logicaloperators.js
@@ -0,0 +1,12 @@
+//logical and and logical or
+//returns true if both operands are true
+
+console.log(true && false);
+console.log(true || false)
+
+let highIncome = true
+let goodCreditScore = true
+
+let eligibleForLoan = highIncome && goodCreditScore
+
+console.log(eligibleForLoan)
