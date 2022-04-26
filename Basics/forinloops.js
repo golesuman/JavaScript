51c59d3e5de508c4f18671eda7e0d8cae0003123
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-26 20:22:16 +0545

    for in loop in JS

---
+++ Basics/forinloops.js
@@ -0,0 +1,14 @@
+const person = {name:'Suman',age:'20',
+};
+
+for (let key in person)
+{
+    console.log(person[key]);
+}
+
+//for in loop
+const arr = [1,2,3]
+for (let i in arr)
+{
+    console.log(i, arr[i]);
+}
