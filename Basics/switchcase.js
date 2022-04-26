938b1ad2093b94dda58f904d42408dc1f9dbb3b0
Suman Gole <60792767+golesuman@users.noreply.github.com>
2022-04-26 20:16:55 +0545

    loops and switch cases

---
+++ Basics/switchcase.js
@@ -0,0 +1,16 @@
+let role = 'guest';
+switch (role){
+    case 'guest':
+        console.log('guest user');
+        break;
+    case 'user':
+        console.log('User');
+        break;
+
+    case 'moderator':
+        console.log('Moderator');
+        break;
+
+    default:
+        console.log('This is default')
+}
