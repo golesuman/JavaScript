9dec1fa1ad354a6311af178463bba5384760d1bc
golesuman <gole35830@gmail.com>
2022-06-18 15:29:46 +0545

    added constructionfunction

---
+++ Objects/constructionfunction.js
@@ -0,0 +1,21 @@
+function createCircle(radius)
+{
+    return {
+        radius, 
+        draw()
+        {
+            console.log('draw');
+        }
+    };
+}
+
+function Circle(radius)
+{
+    this.radius = radius;
+    this.draw = function()
+    {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
