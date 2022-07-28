83abe9b0cd68aed964f8f48f26aa4f2d7ab1902d
golesuman <gole35830@gmail.com>
2022-07-28 22:13:51 +0545

    added array methods

---
+++ Basics/arrays1.js
@@ -0,0 +1,8 @@
+const hobbies = ['sports', 'football', 'volleyball']
+
+for (let hobby of hobbies)
+{
+    console.log(hobby);
+}
+console.log(hobbies.map(hobby => "hobby : " + hobby));
+console.log(hobbies);
